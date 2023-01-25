b92dd9e8e4a0a0cabbccaa99e2f4daffcddbd971
Abdul Jaweed <jdgaming7320@gmail.com>
2023-01-26 05:01:51 +0530

    logger and exception added

---
+++ main.py
@@ -0,0 +1,19 @@
+from insurance.logger import logging
+from insurance.exception import InsuranceException
+import os, sys
+
+def test_logger_and_exception():
+    try:
+        logging.info("Starting the test_logger_and_exception")
+        result = 3 / 0
+        print(result)
+        logging.info("Ending point of the test_logger_and_exception")
+    except Exception as e:
+        logging.debug(str(e))
+        raise InsuranceException(e, sys)
+    
+if __name__ == "__main__":
+    try:
+        test_logger_and_exception()
+    except Exception as e:
+        print(e)
