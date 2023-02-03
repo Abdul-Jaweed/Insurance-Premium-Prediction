b46643e36a9c5fce8891600fcd255cdb3392df93
Abdul Jaweed <jdgaming7320@gmail.com>
2023-02-03 21:25:21 +0530

    data-transformation-updated

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from insurance.entity.config_entity import DataIngestionConfig
 from insurance.entity import config_entity
 from insurance.components.data_ingestion import DataIngestion
 from insurance.components.data_validation import DataValidation
+from insurance.components.data_transformation import DataTransformation
 
 
 # def test_logger_and_exception():
@@ -40,6 +41,15 @@ if __name__ == "__main__":
                                          data_ingestion_artifact = data_ingestion_artifact)
         
         data_validation_artifact = data_validation.initiate_data_validation()
+        
+        
+        
+        # Data Transformation
+        
+        data_transformation_config = config_entity.DataTransformationConfig(training_pipeline_config=training_pipeline_config)
+        data_transformation = DataTransformation(data_transformation_config=data_transformation_config, data_ingestion_artifact=data_ingestion_artifact)
+        
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
 
         
     except Exception as e:
